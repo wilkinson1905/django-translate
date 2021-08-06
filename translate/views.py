09d160a8986c4983bf958a190ba2f175f883e069
wilkinson1905 <hoge@example.com>
2021-08-06 10:06:42 +0900

    first commit

---
+++ translate/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+
+
+def translate(request):
+    if request.method == "POST":
+        text = request.POST["input_area"]
+        with open("API_KEY.txt") as f:
+            API_KEY = f.read()
+        params = {
+            "auth_key": API_KEY,
+            "text": text,
+            "target_lang": 'JA'  # 出力テキストの言語を英語に設定
+        }
+        deepl_request = requests.post(
+            "https://api-free.deepl.com/v2/translate", data=params)
+        result = deepl_request.json()
+        translated_text = result["translations"][0]["text"]
+        print(translated_text)
+        return render(request, 'translate/translate_main.html',
+                      {"output_text": translated_text, "input_text": text})
+
+    else:
+        pass
+
+    return render(request, 'translate/translate_main.html', {})
